7905b7182c1320f3f219c51ab95a8c5a4faa2c6b
innakarpasas <124259083+innakarpasas@users.noreply.github.com>
2023-02-06 16:02:27 +0200

    Add files via upload
    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import hapi from '@hapi/hapi'
+
+const init = async () => {
+  const server = hapi.server({
+    port: 3000,
+    host: 'localhost'
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return 'Hello World!'
+    }
+  })
+
+  await server.start()
+  console.log('Server running on %s', server.info.uri)
+}
+
+process.on('unhandledRejection', (err) => {
+  console.log(err)
+  process.exit(1)
+})
+
+init()
